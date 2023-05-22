7be51daace08e3c2fec1deea6f8ffa49b6f3c4f8
seymasingin <seymasingin@gmail.com>
2023-05-22 21:09:24 +0300

    free api implemented

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { Text, View, TouchableOpacity, Image, Dimensions, SafeAreaView, TextInput, ImageBackground } from 'react-native';
+import { Text, View, TouchableOpacity, Image, Dimensions, SafeAreaView, TextInput, ImageBackground, KeyboardAvoidingView } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import axios from 'axios';
 import { useNavigation } from '@react-navigation/native';
@@ -13,10 +13,7 @@ export default function Home() {
                                     forecast:{forecastday:[{date:'', day:{mintemp_c:0},hour:[{time:"",temp_c:0},{time:"",temp_c:0},{time:"",temp_c:0},{time:"",temp_c:0},{time:"",temp_c:0}]},
                                                           {date:'',day:{mintemp_c:0}},
                                                           {date:'',day:{mintemp_c:0}},
-                                                          {date:'',day:{mintemp_c:0}},
-                                                          {date:'',day:{mintemp_c:0}},
-                                                          {date:'',day:{mintemp_c:0}},
-                                                          {date:'',day:{mintemp_c:0}}  ]}});
+                                                          ]}});
   
   weekdays= ["Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday"]
  
@@ -41,11 +38,11 @@ export default function Home() {
 
     const getWeekContent = weekdays => {
       let content = [];
-      for (let i = 0; i < weekdays.length; i++) {
+      for (let i = 0; i < 3; i++) {
         const item = weekdays[i];
-        content.push(<View style={styles.futday}><Text key={item.id} style={styles.futuredays}>{weekdays[((new Date(city.forecast.forecastday[i].date)).getDay())]}</Text>
-                            <Text key={1} style={styles.future}>{Math.round(city.forecast.forecastday[i].day.maxtemp_c)}</Text>
-                            <Text key={2} style={styles.night}>{Math.round(city.forecast.forecastday[i].day.mintemp_c)}</Text></View>);
+        content.push(<View key={Math.random(10)} style={styles.futday}><Text  style={styles.futuredays}>{weekdays[((new Date(city.forecast.forecastday[i].date)).getDay())]}</Text>
+                            <Text  style={styles.max}>{Math.round(city.forecast.forecastday[i].day.maxtemp_c)}</Text>
+                            <Text  style={styles.min}>{Math.round(city.forecast.forecastday[i].day.mintemp_c)}</Text></View>);
       }
       return content;
     };
@@ -53,16 +50,17 @@ export default function Home() {
     const getDayContent = () => {
       let content = []
       for (let i=0; i<5; i++){
-        content.push(<View style={styles.hour_container}>
-          <Text key={1} style={styles.hour}>{new Date(city.forecast.forecastday[0].hour[i].time).getHours().toString().padStart(2,"0")}</Text>
+        content.push(<View key={Math.random(10)} style={styles.hour_container}>
+          <Text style={styles.hour}>{new Date(city.forecast.forecastday[0].hour[i].time).getHours().toString().padStart(2,"0")}</Text>
         <Text>{city.forecast.forecastday[0].hour[i].temp_c}</Text></View>)
       }
       return content;
     }
 
   return (
-
-    <SafeAreaView style={styles.container}>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={styles.container}>
       <ImageBackground source={getBackgroundImage()} resizeMode="stretch" style={styles.image}>
       <View style={styles.number}>
         <View style={styles.search_container}>
@@ -86,7 +84,8 @@ export default function Home() {
         </TouchableOpacity>   
       </View>
       </ImageBackground>
-    </SafeAreaView>
+   
+    </KeyboardAvoidingView>
   )
 }
 
@@ -94,21 +93,21 @@ const styles= {
   container:{flex:1, flexDirection: 'column'},
   image:{flex:1, height: Dimensions.get('window').height,
           width: Dimensions.get('window').width,},
-  current:{flexDirection: "column", alignItems: 'center', marginTop: 40 },
+  current:{flexDirection: "column", alignItems: 'center', marginTop: 20, },
   current_deg:{ color:'black', fontSize:40, fontWeight:'bold'},
   number:{flex:9 ,margin:10, borderBottomWidth:1, borderColor: 'aliceblue' },
   region:{color:'black',fontSize:35, fontWeight:'bold'},
   search_container:{flexDirection: "row", margin:15, borderWidth: 1, borderRadius: 10, alignItems:'center'},
   search:{ fontWeight:'bold', },
-  future:{color:'white',fontSize:18, fontWeight:'bold', marginRight:45, textAlign:'right'},
-  night:{color:'gainsboro', fontSize:18, textAlign:'right', flexDirection:'column' },
-  futuredays:{marginRight:160, color: 'white',fontSize:18, fontWeight:'bold',textAlign:'left'},
+  max:{color:'white',fontSize:18, fontWeight:'bold', paddingRight:30 },
+  min:{ color:'gainsboro', fontSize:18, justifyContent:'flex-end', paddingRight:10},
+  futuredays:{flexDirection:'column', marginRight:200, color: 'white',fontSize:18, fontWeight:'bold', },
   search_icon:{marginLeft:20, position: "absolute"},
-  hours:{flex:4, flexDirection: "row", margin:10,borderBottomWidth:1, borderColor: 'aliceblue',justifyContent:"space-between" },
+  hours:{flex:4, flexDirection: "row", margin:10,borderBottomWidth:1, borderColor: 'aliceblue',justifyContent:'space-around' },
   hour:{color:'gainsboro',fontSize:18 },
   hour_container:{flexDirection:'column', marginLeft:25, marginRight:25, alignItems:'center', justifyContent:'center'},
-  days:{flex:6,margin:10,borderBottomWidth:1, borderColor: 'aliceblue'  },
-  futday:{flexDirection:'row', },
+  days:{ flex:6,margin:10,borderBottomWidth:1, borderColor: 'aliceblue',justifyContent:'space-around'   },
+  futday:{flexDirection:'row', alignSelf:'flex-end', },
   slider:{flex:1,margin:10, flexDirection: "row", height:60,
   width:Dimensions.get("window").width, 
   alignItems:'center', },
